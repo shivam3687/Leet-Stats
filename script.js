da44ea7082e78337c50430a25257e6015700de90
Shivam Kumar <shivam202kmr@gmail.com>
2026-02-24 00:17:07 +0530

    Update API URL for user profile fetching

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
         searchButton.textContent = "Searching...";
         searchButton.disabled = true;
 
-        const targetUrl = 'https://leetcode-api-faisalshohag.vercel.app/' + username;
+        const targetUrl = 'https://alfa-leetcode-api.onrender.com/userProfile/' + username;
         
         const response = await fetch(targetUrl);
 
